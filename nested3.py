4c9c9ad79dce53f5344a8a85e52d5b499fba3e97
Nandushangrila <nivedr06@gmail.com>
2025-10-12 11:02:10 -0500

    first commit

---
+++ nested3.py
@@ -0,0 +1,31 @@
+print("Select your ride:")
+print("1.Bike")
+print("2. Scooter")
+
+choice=int(input("Enter your choice:"))
+
+if (choice==1):
+    print("What type of bike?")
+    print("1. Scooty/n")
+    print("2. Scooter/n")
+
+
+choice2=int(input("Enter your number 2 choice:"))
+if (choice2==1):
+    print("You have selected scooty!")
+else:
+    print("You have selected scooter!")
+
+if(choice==2):
+    print("What type of car?")
+    print("1. Sedan")
+    print("2. XUV")
+    
+choice3=int(input("Enter your number 3 choice:"))
+
+if (choice3==1):
+    print("You have seleceted the Sudan!")
+else:
+    print("You have selected the XUV")
+
+
